b1229c824df87166b79e2761cf5531ba361b85b1
Anna <annapn5@mail.ru>
2023-01-26 19:23:24 +0600

    исправила2, переделала 3

---
+++ exercise2/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+void SumOfNumsDigit()
+   {
+    Console.WriteLine("Введите целое число: ");
+    string nu = Console.ReadLine();
+    int i = 0;
+    int sum = 0;
+    int count = nu.Length;
+    int[] mas = new int[count];
+    while(i<count) 
+    {
+      char s = nu[i];
+      mas[i] = s - '0';
+      sum = mas[i]+sum;
+      i++;
+    } 
+
+    Console.WriteLine("Сумма цифер в числе " + sum);
+   }  
+  
+   SumOfNumsDigit();
---
+++ exercise3/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+int[] FillArr(int len, int a, int b)
+{
+    Console.WriteLine("Напишите число элементов массива");
+    string g = Console.ReadLine();
+    len = Convert.ToInt32(g);
+    int [] arr = new int [len];
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(a, b);
+    }
+    return arr;
+}
+void Printarr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("I Равно: " + i);
+        Console.WriteLine(arr[i]);
+    }
+}
+int[]array = FillArr(8,1,10);
+Printarr(array);
+
